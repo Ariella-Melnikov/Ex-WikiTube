79f46fd32f52aff7a62a8ae60726980c7f78d22b
Ariella Melnikov <melnikov.ariella@gmail.com>
2024-06-18 20:43:57 +0300

    change in the structur of the code

--- js/service/youtube.service.js
+++ js/service/youtube.service.js
@@ -3,28 +3,31 @@
 const STORAGE_KEY_VIDEO = "videosDB"
 
 const YOUTUBE_API_KEY = "AIzaSyBXbHajYFy9gw9cL7ASSR3yFaFwk9svCHg"
-const VALUE = "beatles"
 
 const CACHE_KEY = "youtubeCache"
 const CACHE_EXPIRY = 2 * 60 * 60 * 1000
 
-const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${YOUTUBE_API_KEY}&q=${VALUE}`
 
-function getVideo() {
-  const video = loadFromStorage(STORAGE_KEY_VIDEO)
-
-  if (video) {
-    return Promise.resolve(video) // Resolve with cached video if available
-  }
-console.log('url', url)
-  return axios.get(url)
-    .then((res) => {
-      const videoData = res.data.items[0] // Assuming you want the first video in the search results
-      saveToStorage(STORAGE_KEY_VIDEO, videoData) // Save fetched video data to storage
-      return videoData // Return the fetched video data
-    })
-    .catch((err) => {
-      console.error("Error fetching video:", err)
-      throw err // Propagate the error
-    })
+function getVideo(value) {
+    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${YOUTUBE_API_KEY}&q=${value}`
+return axios.get(url).then(res=>res.data)
 }
+
+
+//   const video = loadFromStorage(STORAGE_KEY_VIDEO)
+
+//   if (video) {
+//     return Promise.resolve(video) // Resolve with cached video if available
+//   }
+// console.log('url', url)
+//   return axios.get(url)
+//     .then((res) => {
+//       const videoData = res.data.items[0] // Assuming you want the first video in the search results
+//       saveToStorage(STORAGE_KEY_VIDEO, videoData) // Save fetched video data to storage
+//       return videoData // Return the fetched video data
+//     })
+//     .catch((err) => {
+//       console.error("Error fetching video:", err)
+//       throw err // Propagate the error
+//     })
+
--- js/wikitube.controller.js
+++ js/wikitube.controller.js
@@ -3,17 +3,63 @@
 function onInit() {
   console.log("onInit")
   console.log("load video")
+}
 
-  getVideo()
-    .then((video) => {
-      console.log("video:", video)
-      rendervideo(video)
+function onLoad(ev) {
+  ev.preventDefault()
+  const searchVal = ev.target.elements.search.value
+
+  getVideo(searchVal)
+    .then((videos) => {
+      rendervideo(videos.items) // Pass the items array to rendervideo
     })
     .catch((err) => {
       console.log("err:", err)
-      // showErrMessage(err)
     })
 }
+
+function rendervideo(videos) {
+  const elVideoPlayer = document.querySelector(".video-player")
+  const elVidsContainer = document.querySelector(".vids-container")
+
+  // if (!videos || videos.length === 0) {
+  //   elVideoPlayer.innerHTML = "<p>No video available</p>";
+  //   return;
+  // }
+
+  const video = videos[0]
+  const videoId = video.id.videoId
+  const videoTitle = video.snippet.title
+
+  const strHTMLs = videos
+    .map((video) => {
+      const videoId = video.id.videoId
+      const videoTitle = video.snippet.title
+      return `<article class='video-item'>
+                <iframe width="420" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
+                <p>${videoTitle}</p>
+              </article>`
+    })
+    .join("")
+
+  elVideoPlayer.innerHTML = `<iframe width="420" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>`
+  elVidsContainer.innerHTML = strHTMLs
+}
+//   const videoDescription = video.snippet.description
+//   const videoThumbnail = video.snippet.thumbnails.medium.url
+
+//   const iframeHTML = `
+//     <iframe width="420" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
+//   `
+
+//   elVideoPlayer.innerHTML = `
+//     <div class="video-details">
+//       <h2>${videoTitle}</h2>
+//       <p>${videoDescription}</p>
+//       <img src="${videoThumbnail}" alt="${videoTitle}" class="video-thumbnail">
+//     </div>
+//     ${iframeHTML}
+//   `
 
 // function onLoadvideo() {
 //   console.log("load video")
@@ -32,30 +78,3 @@ function onInit() {
 //     // showErrMessage(err)
 // })
 // }
-
-function rendervideo(video) {
-  const elVideoPlayer = document.querySelector(".video-player")
-
-  if (!video) {
-    elVideoPlayer.innerHTML = "<p>No video available</p>"
-    return
-  }
-
-  const videoId = video.id.videoId
-  const videoTitle = video.snippet.title
-  const videoDescription = video.snippet.description
-  const videoThumbnail = video.snippet.thumbnails.medium.url
-
-  const iframeHTML = `
-    <iframe width="420" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
-  `
-
-  elVideoPlayer.innerHTML = `
-    <div class="video-details">
-      <h2>${videoTitle}</h2>
-      <p>${videoDescription}</p>
-      <img src="${videoThumbnail}" alt="${videoTitle}" class="video-thumbnail">
-    </div>
-    ${iframeHTML}
-  `
-}
